4b72013b6b968469ed917ad894da043b971bc2e5
Yossi Shelly <yossi.shelly@gmail.com>
2024-04-08 18:25:29 +0300

    first commit

---
+++ Pallate.Designer.cs
@@ -0,0 +1,239 @@
+﻿using System.Windows.Forms;
+
+namespace GfxLib
+{
+    partial class Pallate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.RedLbl = new System.Windows.Forms.Label();
+            this.GreenLbl = new System.Windows.Forms.Label();
+            this.BlueLbl = new System.Windows.Forms.Label();
+            this.redNum = new System.Windows.Forms.NumericUpDown();
+            this.greenNum = new System.Windows.Forms.NumericUpDown();
+            this.blueNum = new System.Windows.Forms.NumericUpDown();
+            this.selectedColorPb = new System.Windows.Forms.PictureBox();
+            this.RGBPanel = new System.Windows.Forms.Panel();
+            this.AlphaLbl = new System.Windows.Forms.Label();
+            this.AlphaNum = new System.Windows.Forms.NumericUpDown();
+            this.idxNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.redNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedColorPb)).BeginInit();
+            this.RGBPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AlphaNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.colorPanel.Location = new System.Drawing.Point(0, 0);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(497, 510);
+            this.colorPanel.TabIndex = 0;
+            this.colorPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.colorPanel_Paint);
+            this.colorPanel.Resize += new System.EventHandler(this.colorPanel_Resize);
+            // 
+            // RedLbl
+            // 
+            this.RedLbl.AutoSize = true;
+            this.RedLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.RedLbl.ForeColor = System.Drawing.Color.Red;
+            this.RedLbl.Location = new System.Drawing.Point(165, 7);
+            this.RedLbl.Name = "RedLbl";
+            this.RedLbl.Size = new System.Drawing.Size(45, 25);
+            this.RedLbl.TabIndex = 2;
+            this.RedLbl.Text = "Red";
+            this.RedLbl.Visible = false;
+            // 
+            // GreenLbl
+            // 
+            this.GreenLbl.AutoSize = true;
+            this.GreenLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.GreenLbl.ForeColor = System.Drawing.Color.Lime;
+            this.GreenLbl.Location = new System.Drawing.Point(272, 7);
+            this.GreenLbl.Name = "GreenLbl";
+            this.GreenLbl.Size = new System.Drawing.Size(63, 25);
+            this.GreenLbl.TabIndex = 3;
+            this.GreenLbl.Text = "Green";
+            this.GreenLbl.Visible = false;
+            // 
+            // BlueLbl
+            // 
+            this.BlueLbl.AutoSize = true;
+            this.BlueLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BlueLbl.ForeColor = System.Drawing.Color.Blue;
+            this.BlueLbl.Location = new System.Drawing.Point(398, 7);
+            this.BlueLbl.Name = "BlueLbl";
+            this.BlueLbl.Size = new System.Drawing.Size(50, 25);
+            this.BlueLbl.TabIndex = 4;
+            this.BlueLbl.Text = "Blue";
+            this.BlueLbl.Visible = false;
+            // 
+            // redNum
+            // 
+            this.redNum.Location = new System.Drawing.Point(142, 35);
+            this.redNum.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.redNum.Name = "redNum";
+            this.redNum.Size = new System.Drawing.Size(100, 31);
+            this.redNum.TabIndex = 5;
+            this.redNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // greenNum
+            // 
+            this.greenNum.Location = new System.Drawing.Point(262, 35);
+            this.greenNum.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.greenNum.Name = "greenNum";
+            this.greenNum.Size = new System.Drawing.Size(100, 31);
+            this.greenNum.TabIndex = 6;
+            this.greenNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // blueNum
+            // 
+            this.blueNum.Location = new System.Drawing.Point(379, 35);
+            this.blueNum.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.blueNum.Name = "blueNum";
+            this.blueNum.Size = new System.Drawing.Size(100, 31);
+            this.blueNum.TabIndex = 7;
+            this.blueNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // selectedColorPb
+            // 
+            this.selectedColorPb.Location = new System.Drawing.Point(3, 517);
+            this.selectedColorPb.Name = "selectedColorPb";
+            this.selectedColorPb.Size = new System.Drawing.Size(494, 52);
+            this.selectedColorPb.TabIndex = 8;
+            this.selectedColorPb.TabStop = false;
+            this.selectedColorPb.Visible = false;
+            this.selectedColorPb.Resize += new System.EventHandler(this.selectedColorPb_Resize);
+            // 
+            // RGBPanel
+            // 
+            this.RGBPanel.Controls.Add(this.AlphaLbl);
+            this.RGBPanel.Controls.Add(this.AlphaNum);
+            this.RGBPanel.Controls.Add(this.greenNum);
+            this.RGBPanel.Controls.Add(this.RedLbl);
+            this.RGBPanel.Controls.Add(this.blueNum);
+            this.RGBPanel.Controls.Add(this.GreenLbl);
+            this.RGBPanel.Controls.Add(this.BlueLbl);
+            this.RGBPanel.Controls.Add(this.redNum);
+            this.RGBPanel.Location = new System.Drawing.Point(4, 577);
+            this.RGBPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.RGBPanel.Name = "RGBPanel";
+            this.RGBPanel.Size = new System.Drawing.Size(491, 97);
+            this.RGBPanel.TabIndex = 9;
+            this.RGBPanel.Resize += new System.EventHandler(this.RGBPanel_Resize);
+            // 
+            // AlphaLbl
+            // 
+            this.AlphaLbl.AutoSize = true;
+            this.AlphaLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.AlphaLbl.Location = new System.Drawing.Point(40, 7);
+            this.AlphaLbl.Name = "AlphaLbl";
+            this.AlphaLbl.Size = new System.Drawing.Size(51, 25);
+            this.AlphaLbl.TabIndex = 9;
+            this.AlphaLbl.Text = "Alpa";
+            // 
+            // AlphaNum
+            // 
+            this.AlphaNum.Location = new System.Drawing.Point(16, 35);
+            this.AlphaNum.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.AlphaNum.Name = "AlphaNum";
+            this.AlphaNum.Size = new System.Drawing.Size(100, 31);
+            this.AlphaNum.TabIndex = 8;
+            this.AlphaNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // idxNum
+            // 
+            this.idxNum.AutoSize = true;
+            this.idxNum.BackColor = System.Drawing.Color.Transparent;
+            this.idxNum.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.idxNum.Location = new System.Drawing.Point(231, 517);
+            this.idxNum.Name = "idxNum";
+            this.idxNum.Size = new System.Drawing.Size(35, 41);
+            this.idxNum.TabIndex = 10;
+            this.idxNum.Text = "0";
+            // 
+            // Pallate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.idxNum);
+            this.Controls.Add(this.RGBPanel);
+            this.Controls.Add(this.selectedColorPb);
+            this.Controls.Add(this.colorPanel);
+            this.Name = "Pallate";
+            this.Size = new System.Drawing.Size(500, 679);
+            this.Enter += new System.EventHandler(this.Pallate_Enter);
+            this.Resize += new System.EventHandler(this.Pallate_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.redNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedColorPb)).EndInit();
+            this.RGBPanel.ResumeLayout(false);
+            this.RGBPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AlphaNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel colorPanel;
+        private Label RedLbl;
+        private Label GreenLbl;
+        private Label BlueLbl;
+        private NumericUpDown redNum;
+        private NumericUpDown greenNum;
+        private NumericUpDown blueNum;
+        private PictureBox selectedColorPb;
+        private Panel RGBPanel;
+        private Label idxNum;
+        private Label AlphaLbl;
+        private NumericUpDown AlphaNum;
+    }
+}
---
+++ Pallate.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Reflection;
+using System.Windows.Forms;
+
+
+
+namespace GfxLib
+{
+    public partial class Pallate : UserControl
+    {
+
+        public int _NumOfColors = 4;
+        public Color[] Colors = new Color[256];
+        // 0 1 2 3 4  5  6  7
+        byte[] Yarray = { 1, 2, 2, 4, 4, 8, 8, 16, 16 };
+        byte[] Xarray = { 1, 1, 2, 2, 4, 4, 8, 8, 16 };
+        public Color _SelectedColor;
+        public int SelectedColorARGB;
+        public byte _SelectedColorIdx = 0;
+
+        int[] ColorDialogCustomColors;
+        private bool _SelectedColorBox = false;
+        int NumOfCellsX;
+        int NumOfCellsY;
+
+
+        public bool AlphChennel {
+            get
+            {
+                return AlphaNum.Visible;
+            }
+            set
+            {
+                AlphaLbl.Visible= value;
+                AlphaNum.Visible= value;
+            }
+        }
+
+        public event EventHandler<SelectedColorEventArgs> ColorSelected;
+
+        public class SelectedColorEventArgs : EventArgs
+        {
+            public Color Color;
+            public int colorIndex;
+        }
+
+        protected virtual void OnColorSelected (SelectedColorEventArgs e)
+        {
+            EventHandler<SelectedColorEventArgs> eventHandler = ColorSelected;
+
+            if (eventHandler != null)
+            {
+                eventHandler (this, e);
+            }
+        }
+
+
+        byte _Red, _Green, _Blue;
+        public bool RGBVisible
+        {
+            set
+            {
+                redNum.Visible = value;
+                greenNum.Visible = value;
+                blueNum.Visible = value;
+                RedLbl.Visible = value;
+                GreenLbl.Visible = value;
+                BlueLbl.Visible = value;
+            }
+            get { return redNum.Visible; }
+        }
+
+        byte Red => _Red;
+        byte Green => _Green;
+        byte Blue => _Blue;
+
+
+        public Color SelectedColor
+        {
+            get { return _SelectedColor; }
+            set {
+                _SelectedColor = value;
+                SelectedColorARGB = value.ToArgb();
+
+                selectedColorPb.BackColor = _SelectedColor;
+
+                redNum.Value = _SelectedColor.R;
+                greenNum.Value = _SelectedColor.G;
+                blueNum.Value = _SelectedColor.B;
+                AlphaNum.Value = _SelectedColor.A;
+
+            }
+        }
+
+        public byte SelectedColorIdx
+        {
+            get { return _SelectedColorIdx; }
+            set
+            {
+                
+
+
+                if (colorPanel.Controls.Count > 0)
+                {
+                    //if (_SelectedColorIdx < colorPanel.Controls.Count)
+                        DrawBorder((PictureBox)colorPanel.Controls[_SelectedColorIdx], colorPanel.Controls[_SelectedColorIdx].BackColor, 4);
+                    
+                    DrawBorder((PictureBox)colorPanel.Controls[value], Color.Black, 4);
+                }
+
+                _SelectedColorIdx = value;
+                SelectedColor = Colors[value];
+
+                idxNum.ForeColor = Color.FromArgb(255 - _SelectedColor.R, 255 - _SelectedColor.G, 255 - _SelectedColor.B);
+                idxNum.Text = _SelectedColorIdx.ToString();
+                idxNum.BackColor = _SelectedColor;
+             
+
+
+            }
+        }
+        public int NumOfColors
+        {
+            get { return _NumOfColors; }
+            set {
+                _NumOfColors = value;
+                colorPanel.Controls.Clear();
+                colorPanel.Refresh();
+            }
+        }
+
+        public bool SelectedColorBox {
+            set
+            {
+                selectedColorPb.Visible = value;
+                _SelectedColorBox = value;
+            }
+            get
+            {
+                return selectedColorPb.Enabled;
+            }
+        }
+
+        public Pallate()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+          
+
+            idxNum.Top = selectedColorPb.Top + (int)(selectedColorPb.Height - idxNum.Height) / 2;
+            idxNum.Left = selectedColorPb.Left + (int)(selectedColorPb.Width - idxNum.Width) / 2;
+
+
+            for (int i = 0; i < NumOfColors; i++)
+            {
+                    Colors[i] = Color.FromArgb(rnd.Next (255),rnd.Next(255), rnd.Next(255));
+            }
+
+            SelectedColorIdx = 0;
+        }
+
+        private void colorPanel_Resize(object sender, EventArgs e)
+        {
+  
+            Panel panel = ((Panel)sender);
+            panel.Controls.Clear();
+  
+            panel.Invalidate();
+        }
+
+        private void Pallate_Resize(object sender, EventArgs e)
+        {
+            colorPanel.Width = Width; colorPanel.Height = Height - selectedColorPb.Height - RGBPanel.Height - 8;
+            selectedColorPb.Width = Width;
+            selectedColorPb.Left = colorPanel.Left;
+            selectedColorPb.Top = colorPanel.Bottom + 4;
+            RGBPanel.Top = selectedColorPb.Bottom + 4;
+            RGBPanel.Left = colorPanel.Left;
+            RGBPanel.Width = Width;
+
+            idxNum.Top = selectedColorPb.Top + (int)(selectedColorPb.Height - idxNum.Height) / 2;
+            idxNum.Left = selectedColorPb.Left + (int)(selectedColorPb.Width - idxNum.Width) / 2;
+
+            
+
+            colorPanel.Refresh();
+        }
+
+        private void RGBPanel_Resize(object sender, EventArgs e)
+        {
+            Panel RGBPanel = ((Panel)sender);
+            int NumUDWidth,SpaceBetweenNUD;
+
+            if (AlphChennel)
+            {
+                NumUDWidth = (int)(RGBPanel.Width * 0.2f);
+                SpaceBetweenNUD = (int)(RGBPanel.Width * 0.04f);
+
+            }
+            else
+            {
+                NumUDWidth = (int)(RGBPanel.Width * 0.27f);
+                SpaceBetweenNUD = (int)(RGBPanel.Width * 0.05f);
+            }
+
+            redNum.Width = NumUDWidth;
+            greenNum.Width = NumUDWidth;
+            blueNum.Width = NumUDWidth;
+            AlphaNum.Width = NumUDWidth;
+
+
+            redNum.Left = RGBPanel.Left + SpaceBetweenNUD;
+            greenNum.Left = redNum.Right + SpaceBetweenNUD;
+            blueNum.Left = greenNum.Right + SpaceBetweenNUD;
+            AlphaNum.Left = blueNum.Right+ SpaceBetweenNUD;
+
+            RedLbl.Left = redNum.Left + (int)((redNum.Width - RedLbl.Width) / 2);
+            GreenLbl.Left = greenNum.Left + (int)((greenNum.Width - GreenLbl.Width) / 2);
+            BlueLbl.Left = blueNum.Left + (int)((blueNum.Width - BlueLbl.Width) / 2);
+            AlphaLbl.Left = AlphaNum.Left + (int)((AlphaNum.Width - AlphaLbl.Width) / 2);
+        }
+
+        private void selectedColorPb_Resize(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+          //  idxNum.Top = pb.Top + (int)(pb.Height - idxNum.Height)/2;
+         //   idxNum.Left = pb.Left+ (int)(pb.Width - idxNum.Width) / 2;
+        }
+
+
+        private void colorPanel_Paint(object sender, PaintEventArgs e)
+        {
+            int margim = 0;
+            //int RawAndClomn = (int)Math.Sqrt(NumOfColors);
+            int index = (int)(Math.Log(NumOfColors) / Math.Log(2));
+            NumOfCellsX = Xarray[index];
+            NumOfCellsY = Yarray[index];
+
+            for (int y = 0; y < NumOfCellsY; y++)
+            {
+                for (int x = 0; x < NumOfCellsX; x++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Size = new Size(colorPanel.Width / NumOfCellsX- margim, colorPanel.Height / NumOfCellsY- margim);
+                    pictureBox.BackColor = Colors[y * (NumOfCellsX) + x];
+                    pictureBox.MouseClick += ColorMouseClick;
+                    pictureBox.BorderStyle = BorderStyle.FixedSingle;
+                    pictureBox.Tag = y * NumOfCellsX + x;
+                    pictureBox.Location = new Point(colorPanel.Width / NumOfCellsX * x+ margim, colorPanel.Height / NumOfCellsY * y+margim);
+
+                    ControlPaint.DrawBorder(e.Graphics, pictureBox.ClientRectangle, Color.Blue, ButtonBorderStyle.Solid);
+                    ((Panel)sender).Controls.Add(pictureBox);
+                }
+            }
+        }
+
+        private void ColorMouseClick(object sender, MouseEventArgs e)
+        {
+            
+            PictureBox pickedColor = (PictureBox)sender;
+            byte index = byte.Parse(pickedColor.Tag.ToString());
+
+            if (e.Button == MouseButtons.Right && !(pickedColor.Tag.ToString() == "0"))
+            {
+                ColorDialog colorDialog = new ColorDialog();
+
+                if (ColorDialogCustomColors != null)
+                    colorDialog.CustomColors = ColorDialogCustomColors;
+
+                colorDialog.Color = pickedColor.BackColor;
+                colorDialog.FullOpen = true;
+                colorDialog.AnyColor = true;
+                colorDialog.SolidColorOnly = false;
+                colorDialog.ShowDialog();
+                ColorDialogCustomColors = colorDialog.CustomColors;
+                // Color3P.BackColor = colorDialog.Color;
+                pickedColor.BackColor = colorDialog.Color;
+
+                Colors[int.Parse(pickedColor.Tag.ToString())] = pickedColor.BackColor;
+                //Colors[int.Parse((pictureBox.Tag.ToString()))] = pictureBox.BackColor;
+
+            }
+
+            SelectedColorIdx= index;
+
+
+            SelectedColorEventArgs args = new SelectedColorEventArgs();
+            args.Color = SelectedColor;
+            args.colorIndex = index;
+
+            OnColorSelected(args);
+        }
+
+        public void SetPalette(Color[] palette)
+        {
+            NumOfColors= palette.Length;
+            Colors = palette;
+            _SelectedColorIdx = 0;
+        }
+
+        public byte FindColorInPallate (Color color)
+        {
+            byte i = 0;
+            while (i < NumOfColors && Colors[i].ToArgb() != color.ToArgb())
+            {
+              i++;
+            } 
+            return i;
+
+        }
+
+
+        private void Pallate_Enter(object sender, EventArgs e)
+        {
+            SelectedColorIdx = 0;
+
+            SoundPlayer soundPlayer = new SoundPlayer();
+            soundPlayer.Play();
+        }
+
+        private void DrawBorder (PictureBox pb,Color color, int BorderWidth)
+        {
+            Graphics g = pb.CreateGraphics();
+            Pen pen = new Pen(color, BorderWidth);
+            g.DrawRectangle(pen, pb.ClientRectangle);
+            g.Dispose();
+        }
+
+        private void RemoveBorder (PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.Clear(System.Drawing.SystemColors.Control);
+        }
+    }
+}
